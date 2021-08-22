f083626ca54566746f14ec61a1259ed900dbeba6
DanielFadlon-Cherrypick <fadlon@cherrypick-consultaing.com>
2021-08-22 14:43:13 +0300

    request from movie API

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from request import search_movies_by_title, get_movie_by_id
 
+r1 = search_movies_by_title('lion')
+r2 = get_movie_by_id('1')
+r3 = get_movie_by_id('tt0363771')
+
+print(r1)
+print(r2)
+print(r3)
